d4277f63fc66250bd7124a815a20c2c6997b70ee
yehong you <youyehong@gmail.com>
2016-05-13 15:18:26 +1000

    fix url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
     date.strftime('%d %b %Y ,   %l:%M %P')
   end
 
+  def image_tag(source, options={})
+        options = options.symbolize_keys
+
+        src = options[:src] = path_to_image(source)
+
+        unless src =~ /^(?:cid|data):/ || src.blank?
+          options[:alt] = options.fetch(:alt){ image_alt(src) }
+        end
+
+        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+        tag("img", options)
+   end
+
 end
